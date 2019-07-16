92493b007ed0475b8430542e0e4374ed3289b939
robert-hanson <handofthesun@gmail.com>
2019-07-16 18:47:41 -0500

    fixed email issue by updating to do full page submission instead of via ajax. Updated some on shows page. brought icons on contact page up a little

--- routes/contact.js
+++ routes/contact.js
@@ -5,19 +5,19 @@ const ContactManager = require('../managers/ContactManager');
 
 /* GET contact page. */
 router.get('/', function(req, res, next) {
-  res.render('contact');
+  res.render('contact', {commentSaved: null});
 });
 
 // POST sent comment
 router.post('/', async(req, res)=>{
-    console.log(JSON.stringify(req.body.date));
+    console.log(JSON.stringify(req.body));
     try {
         const name = req.body.name;
         const email = req.body.email;
         const message = req.body.message;
-        const date = req.body.date;
+        const date = new Date();
         await ContactManager.saveContactMessage(name, email, message, date);
-        res.send({error: null});
+        res.render('contact', {commentSaved: true});
     } catch(ex) {
         console.error('Something went wrong :(...\nError:' + JSON.stringify(ex));
         res.send({error: ex});
